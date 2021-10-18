99bb2e19520f994fac15f2ac19b64877affb4136
Joao Pedro R <jaop123456789@hotmail.com>
2021-10-18 00:11:41 -0300

    Ciclo

---
+++ CICLO 2/Desafio 02/Script02.js
@@ -0,0 +1,39 @@
+window.onload = function () {
+ 
+ 
+    function resultadoCep(cepDoMeAche) {
+ 
+       for (let campo in cepDoMeAche) {
+          if (document.querySelector(`#${campo}`))
+             document.querySelector(`#${campo}`).value = cepDoMeAche[campo];
+ 
+       }
+ 
+ 
+    }
+ 
+
+    let cepDoMeAche = async function (cep) {
+       let url = `https://viacep.com.br/ws/${cep}/json/`;
+       try {
+          let dadosRua = await fetch(url);
+          let dadosJson = await dadosRua.json();
+          resultadoCep(dadosJson);
+
+       } 
+       catch (error) {
+          alert("CEP errado ou inexistente");
+       }
+ 
+    }
+ 
+    const btBuscar = document.querySelector("#boton");
+    const cepp = document.querySelector("#ceep");
+    btBuscar.addEventListener(`click`, function () {
+ 
+       cepDoMeAche(cepp.value);
+ 
+    })
+ 
+ 
+ }
